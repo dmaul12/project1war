9d5bed084d23dd00f1dd4ef668edcfdb6d6ad63f
David Maul <dmaul12@gmail.com>
2016-06-12 14:08:42 -0400

    added score and tiebreaker

--- starter.js
+++ starter.js
@@ -2,15 +2,16 @@
 
 $(document).ready(function(){
   console.log("loaded");
-  // var J = 11
-  // var Q = 12
-  // var K = 13
-  // var A = 14
+  var J = "11"
+  var Q = "12"
+  var K = "13"
+  var A = "14"
 var suits = ["H", "S", "D", "C"]; //image here?
 // var values = [2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A];
-var values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+var values = [A, "2", "3", "4", "5", "6", "7", "8", "9", "10", J, Q, K];
 var fullDeck =[];
-
+var playerScore = 0;
+var cScore = 0;
 
 for (var i = 0; i< suits.length; i++) {
   values.forEach(function(values){
@@ -23,7 +24,7 @@ $('#button').click(shuffleCards)
 function shuffleCards() {
   var i, j;
   var temp;
-
+//fisher-yates
   for(var i = fullDeck.length -1; i >0; i--){
     var j = Math.floor(Math.random()* i);
     temp = fullDeck[i];
@@ -37,6 +38,8 @@ shuffleCards();
 
 cpu = fullDeck.slice(0, 26);
 player = fullDeck.slice(26);
+
+
 // console.log(cpu);
 // console.log(player);
 
@@ -44,25 +47,55 @@ $('#deal').click(compareCards);
 
 
 function compareCards(){
-  card1= cpu.shift();
+
+document.getElementById('pactual').innerHTML=playerScore;
+document.getElementById('cactual').innerHTML=cScore;
+ card1= cpu.shift();
  card2= player.shift();
   if(parseInt(card1)>parseInt(card2)){
-    alert("computer wins")
+    cScore=cScore+1;
+  document.getElementById('player').innerHTML = player[0];
+  document.getElementById('cpu').innerHTML = cpu[0];
+    document.getElementById('console').innerHTML="Computer wins hand"
       }else if(parseInt(card2)>parseInt(card1)){
-      alert("player wins")
+      //point player
+      playerScore=playerScore+1;
+  document.getElementById('player').innerHTML = player[0];
+  document.getElementById('cpu').innerHTML = cpu[0];
+     document.getElementById('console').innerHTML="Player wins hand"
      }
-     console.log(cpu);
-console.log(player);
-// else{
-//       alert("tie breaker")
-//         return tieBreaker;
-//       }
-//       }
-// function tieBreaker(){
-//   //append "I declare war" on screen
-//   return false
-}
 
+else{
+      document.getElementById('console').innerHTML="Tie Breaker!"
+        return tieBreaker;
+      }
+       console.log(cpu);
+     console.log(player);
+      }
+function tieBreaker(){
+  //append "I declare war" on screen
+ card1= cpu.splice(0,2);
+ card2= player.splice(0,2);
+    if(parseInt(card1)>parseInt(card2)){
+    // point cpu
+    cScore=cScore+1;
+  document.getElementById('player').innerHTML = player[0];
+  document.getElementById('cpu').innerHTML = cpu[0];
+    document.getElementById('console').innerHTML="Computer wins hand"
+      }else if(parseInt(card2)>parseInt(card1)){
+      //point player
+      playerScore=playerScore+1
+  document.getElementById('player').innerHTML = player[0];
+  document.getElementById('cpu').innerHTML = cpu[0];
+     document.getElementById('console').innerHTML="Player wins hand"
+     }
+
+else{
+      document.getElementById('console').innerHTML="Tie Breaker!"
+        return tieBreaker;
+      }
+}
+compareCards();
 
 
 
